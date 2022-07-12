336ccf4aaaed81a492ba4b40016ac39cf2944111
HayotbekIT <103405668+HayotbekIT@users.noreply.github.com>
2022-07-12 08:38:21 +0500

    Changed SASS
    Было изменино SCSS

--- gulpfile.js
+++ gulpfile.js
@@ -1,27 +1,33 @@
 const { task, src, dest, series, parallel, watch } = require("gulp");
 const del = require("del");
 const pug = require("gulp-pug");
+const sourcemaps = require("gulp-sourcemaps");
+const sass = require("gulp-sass")(require("sass"));
 
 task("clear", function (callback) {
   return del("./build");
   callback();
 });
 task("pug", function (callback) {
-  return src("./source/pug/**/index.pug").pipe(pug({
-    pretty: true
-  })).pipe(dest("./build/"));
+  return src("./source/pug/**/index.pug")
+    .pipe(
+      pug({
+        pretty: true,
+      })
+    )
+    .pipe(dest("./build/"));
   callback();
 });
 task("css", (callback) => {
-  return src("./source/css/**/*.css").pipe(dest("./build/css"));
-  callback();
-});
-task("map", (callback) => {
-  return src("./source/css/**/*.css.map").pipe(dest("./build/css"));
+  return src("./source/scss/main.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write("map"))
+    .pipe(dest("./build/css"));
   callback();
 });
 task("scss", (callback) => {
-  return src("./source/sass/**/*.scss").pipe(dest("./build/sass"));
+  return src("./source/scss/**/*.scss").pipe(dest("./build/sass"));
   callback();
 });
 task("image", (callback) => {
@@ -35,9 +41,8 @@ task("js", (callback) => {
 
 task("watch", function () {
   watch("./source/pug/**/*.pug", parallel("pug"));
-  watch("./source/css/**/*.*", parallel("css"));
-  watch("./source/css/**/*.*", parallel("map"));
-  watch("./source/sass/**/*.scss", parallel("scss"));
+  watch("./source/scss/**/*.*", parallel("css"));
+  watch("./source/scss/**/*.scss", parallel("scss"));
   watch("./source/img/**/*.*", parallel("image"));
   watch("./source/js/**/*.js", parallel("js"));
 });
@@ -47,6 +52,6 @@ task(
   series(
     parallel("clear"),
     parallel("pug"),
-    parallel("watch", "css","map", "scss", "image", "js")
+    parallel("watch","css", "scss", "image", "js")
   )
 );
