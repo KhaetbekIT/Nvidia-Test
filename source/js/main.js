6c831d0b7093d4d0631cbce8db24f986beec396d
Khaetbek <hayotbek-web@mail.ru>
2023-03-23 20:20:16 +0500

    #fix #clone #project Клонировал проект для заного разраба

---
+++ source/js/main.js
@@ -0,0 +1,23 @@
+// Для добавления функционала используем классы с префиксом js, стилизовать по этим классам нельзя
+
+$(document).ready(function(){
+
+	// $('input[type=tel]')
+	// 	.inputmask("8 (999) 999 99 99");
+
+
+	// $('.js-popup-img')
+	// 	.magnificPopup({
+	// 		type:'image',
+	// 		closeOnContentClick: true,
+	// 		fixedContentPos: true,
+	// 		mainClass: 'mfp-no-margins mfp-with-zoom',
+	// 		image: {
+	// 			verticalFit: true
+	// 		},
+	// 		zoom: {
+	// 			enabled: true,
+	// 			duration: 300
+	// 		}
+	// 	});
+});
