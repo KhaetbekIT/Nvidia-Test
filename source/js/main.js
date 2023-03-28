b0871e8612becff4474ee797137295876ae20a4d
Khaetbek <hayotbek-web@mail.ru>
2023-03-28 20:24:00 +0500

    #fix #js #func Написал код внутрии функциий

--- source/js/main.js
+++ source/js/main.js
@@ -1,8 +1,12 @@
-const navbarMobile = document.querySelector("#navbar-mobile-js")
-const navbarBurger = document.querySelector("#navbar-burger-js")
+const Navigation = () => {
+	const navbarMobile = document.querySelector("#navbar-mobile-js")
+	const navbarBurger = document.querySelector("#navbar-burger-js")
 
-navbarBurger.addEventListener("click", ()=>{
-	navbarBurger.classList.toggle("active")
-	navbarMobile.classList.toggle("hidden")
-	document.body.classList.toggle("no-scroll")
-})
+	navbarBurger.addEventListener("click", () => {
+		navbarBurger.classList.toggle("active")
+		navbarMobile.classList.toggle("hidden")
+		document.body.classList.toggle("no-scroll")
+	})
+}
+
+Navigation()
